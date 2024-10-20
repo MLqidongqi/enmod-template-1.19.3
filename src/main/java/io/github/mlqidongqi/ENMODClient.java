118b95feacd62ccb3dfac6d4e1cf281d13189943
MC_XYR <ds3ded@163.com>
2024-10-20 21:33:03 +0800

    Initial commit

---
+++ src/main/java/io/github/mlqidongqi/ENMODClient.java
@@ -0,0 +1,10 @@
+package io.github.mlqidongqi;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ENMODClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/io/github/mlqidongqi/ENMODDataGenerator.java
@@ -0,0 +1,11 @@
+package io.github.mlqidongqi;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ENMODDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
