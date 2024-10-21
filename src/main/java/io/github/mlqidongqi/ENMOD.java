e4d28da7f4ec4558d76f468ac0308e10b723649d
MC_XYR <ds3ded@163.com>
2024-10-21 21:42:56 +0800

    Change log text

--- src/main/java/io/github/mlqidongqi/ENMOD.java
+++ src/main/java/io/github/mlqidongqi/ENMOD.java
@@ -1,16 +1,16 @@
 package io.github.mlqidongqi;
 
+import io.github.mlqidongqi.registry.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class ENMOD implements ModInitializer {
-	public static final String MOD_ID = "enmod";
-
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
+	public static final String MOD_ID = "enmod";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
@@ -20,5 +20,6 @@ public class ENMOD implements ModInitializer {
 		// Proceed with mild caution.
 
 		LOGGER.info("Hello world!");
+		ModItems.registerMODItems();
 	}
 }
---
+++ src/main/java/io/github/mlqidongqi/registry/ModItems.java
@@ -0,0 +1,25 @@
+package io.github.mlqidongqi.registry;
+
+import io.github.mlqidongqi.ENMOD;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+    public static final Item SIATEMENT = registerItem("siatement",
+            new Item(new FabricItemSettings()));
+    public static Item registerItem(String name, Item item, ItemGroup... itemGroups) {
+        Item registerItem = Registry.register(Registries.ITEM, new Identifier(ENMOD.MOD_ID, name), item);
+        for (ItemGroup itemGroup: itemGroups) {
+            ItemGroupEvents.modifyEntriesEvent(itemGroup).register(entries -> entries.add(registerItem));
+        }
+        return registerItem;
+    }
+    public static void registerMODItems() {
+        ENMOD.LOGGER.debug("Registering mod items for" + ENMOD.MOD_ID);
+    }
+}
